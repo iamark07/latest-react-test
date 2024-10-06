f61eb9d263e3970bc8dd10f30ad0ccf31d1eba7f
Arbaz Karimi <arbazkarimi@gmail.com>
2024-10-06 22:17:11 +0530

    update color

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <>
-      <h1 className='text-red-500 text-9xl bg-zinc-900'>latest test</h1>
+      <h1 className='text-yellow-500 text-9xl bg-zinc-900'>latest test</h1>
     </>
   )
 }
